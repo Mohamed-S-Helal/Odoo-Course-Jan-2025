c6a83d623df62ada49d11d65aecd16ef3f23bf87
Mohamed Salah <mohamed.salah@intalio.com>
2025-02-13 19:42:20 +0200

    SchoolManagement

--- SchoolManagement/school_management/models/school_teacher.py
+++ SchoolManagement/school_management/models/school_teacher.py
@@ -1,9 +1,12 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from dateutil.relativedelta import relativedelta
 
 
 class SchoolTeacher(models.Model):
     _name = 'school.teacher'
     _description = 'This is a teacher model'
+
+    personal_photo = fields.Image(max_width=50)
 
     name = fields.Char()
 
@@ -13,6 +16,29 @@ class SchoolTeacher(models.Model):
     ])
 
     school_id = fields.Many2one('school.main')
+
+    teacher_stage = fields.Selection(related='school_id.stage', readonly=False, store=True)
+
+    birth_date = fields.Date()
+
+    age = fields.Integer(compute='_compute_age', store=True)
+
+    # graduation_year
+
+    # experience_years
+
+    # experience_months
+
+    @api.depends('birth_date')
+    def _compute_age(self):
+        # self -> recordset
+
+        for record in self:
+            if record.birth_date:
+                today = fields.Date.today()
+                record.age = relativedelta(today, record.birth_date).years
+            else:
+                record.age = 25
 
     classes_ids = fields.Many2many(comodel_name='school.class',
                                    relation='teacher_class_rel',
